4533c1c681d56e1b2ea1b301cd791b91ee430506
Smooth X Dev <161130060+Barlosschkldsi111@users.noreply.github.com>
2026-04-01 18:47:52 +0700

    Update Load.lua

--- Load.lua
+++ Load.lua
@@ -1,19 +1,14 @@
-
 local api = loadstring(game:HttpGet("https://sdkapi-public.luarmor.net/library.lua"))()
-
-local function CheckLoad()
-    if game.GameId == 124473577469410 then -- Be a Lucky Block
-        return "14f25102b0fee02ece86213aa3a6a525"
-    elseif game.GameId == 77747658251236 then -- Sailor Piece
-        return "69ba33a41922aed482b220b9e7f7c6bb"
-    end 
-end 
-
-api.script_id = CheckLoad()
-
+local function GetScriptID()
+    local GameMap = {
+        [9186719164] = "69ba33a41922aed482b220b9e7f7c6bb", -- Sailor Piece
+        [9787206684] = "14f25102b0fee02ece86213aa3a6a525" -- Be a Lucky Block
+    }
+    return GameMap[game.GameId]
+end
+local scriptId = CheckLoad()
+api.script_id = scriptId
 local status = api.check_key(script_key)
-
-if (status.code == "KEY_VALID") then
-    script_key = script_key
-    api.load_script();
+if status and status.code == "KEY_VALID" then
+    api.load_script()
 end
